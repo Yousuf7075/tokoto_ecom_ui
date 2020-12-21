4b42f046192014222a7ccd28d649ac925ded79f6
Aaraj Yousuf <aarajyousufdiu@gmail.com>
2020-12-21 13:06:52 +0600

    refactor code

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/widgets.dart';
-import 'file:///D:/Flutter/My_Project/tokoto_ecom/lib/screens/splash/splash_screen.dart';
+import 'package:tokoto_ecom/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:tokoto_ecom/screens/splash//components/body.dart';
+import 'package:tokoto_ecom/screens/splash/components/body.dart';
 import 'package:tokoto_ecom/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
-  static String routeName = "/splash";
+  static String routeName = "/screens.splash";
   @override
   Widget build(BuildContext context) {
     // You have to call it on your starting screen
